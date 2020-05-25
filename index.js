f0b35097a9e163a891cecd5065f6965b93b568f5
jenny <gaoyz@xiaomawang>
2020-05-25 16:23:46 +0800

    post data

--- index.js
+++ index.js
@@ -2,20 +2,49 @@ const http = require('http')
 const querystring = require('querystring')
 
 const server = http.createServer((req,res)=>{
-    console.log(req.method)
+    const method = req.method
     const url = req.url
-    console.log('url---',url)
-    // req.query = querystring.parse(url.split('?')[1])
-    const path = querystring.parse(url.split('?')[0]) // 路由
+    const path = url.split('?')[0]
+    const query = querystring.parse(url.split('?')[1])
+    // 设置返回格式 JSON
+    res.setHeader('Content-type','application/json') // 
+
+    // 返回数据
+    const resData = {
+        method,
+        url,
+        path,
+        query
+    }
+    if(method === "GET"){
+        res.end(JSON.stringify(resData)) // json格式字符串
+    }
+    if(method === "POST"){
+        let postData = ''
+        req.on('data',chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end',()=>{
+            resData.postData = postData
+            res.end(JSON.stringify(resData))
+        })
+    }
     
-    console.log('req.path---',path)
-    // res.writeHead(200,{'content-type':'text/html'})
-    // res.end(JSON.stringify(req.query))
-    res.end(path)
+    // console.log(req.method)
+    // const url = req.url
+    // console.log('url---',url)
+    // // req.query = querystring.parse(url.split('?')[1])
+    // const path = url.split('?')[0] // 路由
+    
+    // console.log('req.path---',path)
+    // // res.writeHead(200,{'content-type':'text/html'})
+    // // res.end(JSON.stringify(req.query))
+    // res.end(path)
+
 })
 
 server.listen(3002,()=>{
-    console.log('listening on 3002 port')
+    console.log('listening on 3002 port1212')
 })
 
 
--- testPost.js
+++ testPost.js
@@ -1,25 +1,25 @@
-const http = require('http')
-const querystring = require('querystring')
+// const http = require('http')
+// const querystring = require('querystring')
 
-const server = http.createServer((req,res)=>{
-    if(req.method === 'POST') {
-        // req 数据格式
-        console.log('content-type',req.headers['content-type'])
-        // 接收数据
-        let postData =''
-        req.on('data',chunk =>{
-            postData += chunk.toString()
-        })
-        req.on('end',()=>{
-            console.log('end--postData',postData)
-            res.end('end hello')
-        })
-    }
+// const server = http.createServer((req,res)=>{
+    // if(req.method === 'POST') {
+    //     // req 数据格式
+    //     console.log('content-type',req.headers['content-type'])
+    //     // 接收数据
+    //     let postData =''
+    //     req.on('data',chunk =>{
+    //         postData += chunk.toString()
+    //     })
+    //     req.on('end',()=>{
+    //         console.log('end--postData',postData)
+    //         res.end('end hello')
+    //     })
+    // }
     
-})
+// })
 
-server.listen(3002,()=>{
-    console.log('listening on 3002 port')
-})
+// server.listen(3002,()=>{
+//     console.log('listening on 3002 port')
+// })
 
 
