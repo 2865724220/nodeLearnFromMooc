b013df2f24a23ce1518c7dc7825976083de0c993
jenny <gaoyz@xiaomawang>
2020-05-25 16:03:47 +0800

    add fun

--- b.js
+++ b.js
@@ -8,3 +8,11 @@ console.log(sum,result)
 
 const arr = _.concat([1,2,3])
 console.log('arr---',arr)
+
+// /api/blog/list get author,keyword,参数为空，不过滤
+// /api/blog/detail get id
+// /api/blog/view
+// /api/blog/new
+// /api/blog/update
+// /api/blog/del
+// /api/user/login username,password
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const http = require('http')
+const querystring = require('querystring')
 
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'})
-    res.end('<h1>hello world</h1>')
+    console.log(req.method)
+    const url = req.url
+    console.log('url---',url)
+    // req.query = querystring.parse(url.split('?')[1])
+    const path = querystring.parse(url.split('?')[0]) // 路由
+    
+    console.log('req.path---',path)
+    // res.writeHead(200,{'content-type':'text/html'})
+    // res.end(JSON.stringify(req.query))
+    res.end(path)
 })
 
 server.listen(3002,()=>{
     console.log('listening on 3002 port')
 })
+
+
